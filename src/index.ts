9516c5724f2f68ced95d9d4d9d4b152c8be34a16
Jacoby-Y <cobyyliniemi@gmail.com>
2024-01-10 10:37:28 -0600

    Attempt to fix importing issues

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import Signal from "./Signal/Signal";
+import * as CobysUtils from "./CobysUtils/CobysUtils";
+import * as Runner from "./Runner/Runner";
+
 export default {
-    msg: "Hello World!"
+    Signal,
+    CobysUtils,
+    Runner
 }
