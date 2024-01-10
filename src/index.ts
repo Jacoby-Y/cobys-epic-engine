bf37a0985efa1175e36986d1c7958746227be891
Jacoby-Y <cobyyliniemi@gmail.com>
2024-01-10 10:43:28 -0600

    Fix index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import Signal from "./Signal/Signal";
 import * as CobysUtils from "./CobysUtils/CobysUtils";
 import * as Runner from "./Runner/Runner";
 
-export default {
-    Signal,
-    CobysUtils,
-    Runner
-}
+export const signal = Signal;
+export const cobysUtils = CobysUtils;
+export const runner = Runner;
